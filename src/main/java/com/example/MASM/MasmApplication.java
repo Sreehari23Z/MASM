9c64181aebcc8824516c245eaf7413cfe13ff744
Sreehari23Z <sreeharisaji2003@gmail.com>
2024-09-28 15:44:33 +0530

    backend init

---
+++ src/main/java/com/example/MASM/MasmApplication.java
@@ -0,0 +1,13 @@
+package com.example.MASM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MASM/MasmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MASM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
